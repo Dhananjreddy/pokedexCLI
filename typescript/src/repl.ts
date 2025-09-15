b5e381c95f7a42917e8ab8c5ebe8332359617499
dhananjreddy <dhananjreddy@gmail.com>
2025-09-15 08:33:42 +0000

    REPL loop

--- typescript/src/repl.ts
+++ typescript/src/repl.ts
@@ -1,4 +1,26 @@
+import  { createInterface } from 'node:readline';
+
 export function cleanInput(input: string): string[] {
     const words = input.trim().toLowerCase().split(/\s+/)
     return words
+}
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Pokedex > "
+});
+
+export function startREPL(): void {
+    rl.prompt(); 
+
+    rl.on("line", (line: string) => {
+        const input = cleanInput(line);
+        if (input.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log("Your command was:", input[0]);
+        rl.prompt();
+    })
 }
